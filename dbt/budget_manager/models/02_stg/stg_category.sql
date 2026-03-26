383bdb90bf98f3a48df4bf1338e25170f688a58d
TVelev <61898409+de-prox@users.noreply.github.com>
2026-03-26 20:46:51 +0200

    create stage models
    Models are materialized as views, because the data I'm working with
    is small.
    
    Each stage model selects the data from the source without any major
    transformations, because they are not needed. Just casting to
    ensure the right data type for the tables in the warehouse.
    
    Created also the properties.yaml with models configs and tests.

--- dbt/budget_manager/models/02_stg/stg_category.sql
+++ dbt/budget_manager/models/02_stg/stg_category.sql
@@ -1,9 +1,9 @@
 with
 
-    CATEGORY as ( select 
+    CATEGORY as ( select
                          cast( c.id                as VARCHAR(100) )   as category_id_bk   ,
                          cast( c.title             as VARCHAR(50)  )   as category_name    ,
-                         cast( c.type              as VARCHAR(20)  )   as category_type    ,
+                         cast( c.type              as VARCHAR(20)  )   as category_type    , -- [Приход, Разход]
                          ---
                          cast( c.is_archived       as VARCHAR(5)   )   as is_archived      ,
                          cast( c.created_time      as TIMESTAMP    )   as created_time     ,
@@ -15,5 +15,6 @@ with
 --------------------------------------------------------------
 -- MAIN QRY
 --------------------------------------------------------------
-select *
-from CATEGORY t
+select t.*
+       ---
+from   CATEGORY t
--- dbt/budget_manager/models/02_stg/stg_year.sql
+++ dbt/budget_manager/models/02_stg/stg_year.sql
@@ -1,11 +1,11 @@
 with
 
-    YEAR as ( select 
+    YEAR as ( select
                      cast( y.id                as VARCHAR(100) )   as year_id_bk       ,
                      cast( y.title             as VARCHAR(4)   )   as year_name        ,
                      ---
                      cast( y.created_time      as TIMESTAMP    )   as created_time     ,
-                     cast( y.last_edited_time  as TIMESTAMP    )   as last_edited_time 
+                     cast( y.last_edited_time  as TIMESTAMP    )   as last_edited_time
                      ---
               from   {{ source('01_src', 'year') }} y
              )
@@ -13,5 +13,6 @@ with
 --------------------------------------------------------------
 -- MAIN QRY
 --------------------------------------------------------------
-select *
-from YEAR t
+select t.*
+       ---
+from   YEAR t
