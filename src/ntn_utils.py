494b2ed253328466529bd8647103bb0a0d2d0143
t-velev <61898409+de-prox@users.noreply.github.com>
2026-05-02 16:17:41 +0300

    refactor exception handling code

--- src/ntn_utils.py
+++ src/ntn_utils.py
@@ -173,11 +173,9 @@ def get_last_load_date(schema_name: str, table_name: str, engine) -> datetime:
     query = f'select max(load_date) from {schema_name}.{table_name}'
 
     try:
-
         df = pd.read_sql_query(query, engine)
 
     except (pd.errors.DatabaseError, SQLAlchemyError) as e:
-
         print(f'Error: Could not query {schema_name}.{table_name}. Details: {e}')
         raise
 
@@ -205,37 +203,32 @@ def load_new_data(schema_name: str, table_name: str, new_data_df, engine) -> int
         int: The number of rows affected.
     """
 
-    with engine.begin() as conn:
+    try:
+        with engine.begin() as conn:
 
-        if len(new_data_df) > 0:
+            if len(new_data_df) > 0:
 
-            # Transform the Notion IDs to a list
-            ids_list = new_data_df['id'].tolist()
+                # Transform the Notion IDs to a list
+                ids_list = new_data_df['id'].tolist()
 
-            # DELETE records from the raw table by using new records IDs if they are already there
-            query = text(f'DELETE from {schema_name}.{table_name} where id in :id_list')
+                # DELETE records from the raw table by using new records IDs if they are already there
+                query = text(f'DELETE from {schema_name}.{table_name} where id in :id_list')
 
-            try:
                 # Execute the DELETE using a named parameter :id_list and passing the values in a dictionary,
                 # because a tuple with one value has a trailing comma and it breaks a standard query statement.
                 conn.execute(query, {'id_list': tuple(ids_list)})
 
-            except SQLAlchemyError as e:
-                print(f'Error: Could not DELETE from {schema_name}.{table_name}. Details: {e}')
-                raise
-
-            try:
                 # INSERT the new records to the raw table
                 result = new_data_df.to_sql(name=table_name, con=conn, schema=schema_name, if_exists='append', index=False, method='multi', chunksize=1000)
 
-            except (pd.errors.DatabaseError, SQLAlchemyError) as e:
-                print(f'Error: Could not INSERT into {schema_name}.{table_name}. Details: {e}')
-                raise
+            else:
+                result = 0
 
-        else:
-            result = 0
+            return result
 
-        return result
+    except (pd.errors.DatabaseError, SQLAlchemyError) as e:
+        print(f'Error: The load new data process failed for {schema_name}.{table_name}. Details: {e}')
+        raise
 
 
 def del_missing_data(schema_name: str, table_name: str, filtered_df, engine) -> int:
@@ -257,37 +250,25 @@ def del_missing_data(schema_name: str, table_name: str, filtered_df, engine) -> 
         int: How many rows were actually deleted.
     """
 
-    with engine.begin() as conn:
+    try:
+        with engine.begin() as conn:
 
-        # DELETE the ID records from previous runs so we can insert the most current ones
-        query = text(f"DELETE from {schema_name}.notion_ids_audit where source_name = '{table_name}'")
-
-        try:
+            # DELETE the ID records from previous runs so we can insert the most current ones
+            query = text(f"DELETE from {schema_name}.notion_ids_audit where source_name = '{table_name}'")
             conn.execute(query)
 
-        except SQLAlchemyError as e:
-            print(f'Error: Could not DELETE the prev IDs for {table_name} from {schema_name}.notion_ids_audit. Details: {e}')
-            raise
-
-        try:
             # INSERT the most current IDs into notion_ids_audit table
             filtered_df.to_sql(name='notion_ids_audit', con=conn, schema=schema_name, if_exists='append', index=False, method='multi', chunksize=1000)
 
-        except (pd.errors.DatabaseError, SQLAlchemyError) as e:
-            print(f'Error: Could not INSERT the most current IDs of {table_name} into {schema_name}.notion_ids_audit. Details: {e}')
-            raise
-
-        # Use the IDs in NOTION_IDS_AUDIT to find and DELETE the missing rows
-        query = text(f"DELETE from {schema_name}.{table_name} t where not exists (select 1 from {schema_name}.notion_ids_audit tt where tt.id = t.id and tt.source_name = '{table_name}')")
-
-        try:
+            # Use the IDs in NOTION_IDS_AUDIT to find and DELETE the missing rows
+            query = text(f"DELETE from {schema_name}.{table_name} t where not exists (select 1 from {schema_name}.notion_ids_audit tt where tt.id = t.id and tt.source_name = '{table_name}')")
             result = conn.execute(query)
 
-        except SQLAlchemyError as e:
-            print(f'Error: Could not DELETE the missing Notion rows from {schema_name}.{table_name}. Details: {e}')
-            raise
+            return result.rowcount # Return how many rows were actually deleted
 
-        return result.rowcount # Return how many rows were actually deleted
+    except (pd.errors.DatabaseError, SQLAlchemyError) as e:
+        print(f'Error: The delete missing data process failed for {schema_name}.{table_name}. Details: {e}')
+        raise
 
 
 def upsert_into_stats(engine, row_count: int, run_id: int, run_date: datetime, dag_name: str, task_name: str, column: str) -> None:
@@ -327,48 +308,37 @@ def upsert_into_stats(engine, row_count: int, run_id: int, run_date: datetime, d
         schema="warehouse"
     )
 
-    with engine.connect() as conn:
+    try:
+        with engine.begin() as conn:
 
-        # Check if a row with the current dag run_id, name and task exists
-        select_stmt = select(stats_table).where( stats_table.c.run_id    == run_id,
-                                                 stats_table.c.dag_name  == dag_name,
-                                                 stats_table.c.task_name == task_name
-                                                )
-        try:
+            # Check if a row with the current dag run_id, name and task exists
+            select_stmt = select(stats_table).where( stats_table.c.run_id    == run_id,
+                                                     stats_table.c.dag_name  == dag_name,
+                                                     stats_table.c.task_name == task_name
+                                                     )
             select_result = conn.execute(select_stmt).fetchone()
-        except SQLAlchemyError as e:
-            print(f'Error: Could not execute the SELECT statement. Details: {e}')
-            raise
 
-        # If no row exists, create one. Else, update the values
-        if not select_result:
-            insert_stmt = ( insert(stats_table)
-                           .values({ stats_table.c.run_id:    run_id,
-                                     stats_table.c.run_date:  run_date,
-                                     stats_table.c.dag_name:  dag_name,
-                                     stats_table.c.task_name: task_name,
-                                     stats_table.c[column]:   row_count
-                                   })
-                          )
-            try:
+            # If no row exists, create one. Else, update the values
+            if not select_result:
+                insert_stmt = ( insert(stats_table)
+                               .values({ stats_table.c.run_id:    run_id,
+                                         stats_table.c.run_date:  run_date,
+                                         stats_table.c.dag_name:  dag_name,
+                                         stats_table.c.task_name: task_name,
+                                         stats_table.c[column]:   row_count
+                                       })
+                               )
                 insert_result = conn.execute(insert_stmt)
-            except SQLAlchemyError as e:
-                print(f'Error: Could not INSERT a new run_id row in sys_etl_stats. Details: {e}')
-                raise
             else:
-                conn.commit()
-        else:
-            update_stmt = ( update(stats_table)
-                           .where(stats_table.c.run_id    == run_id,
-                                  stats_table.c.run_date  == run_date,
-                                  stats_table.c.dag_name  == dag_name,
-                                  stats_table.c.task_name == task_name)
-                           .values({ stats_table.c[column]: row_count })
-                          )
-            try:
+                update_stmt = ( update(stats_table)
+                               .where( stats_table.c.run_id    == run_id,
+                                       stats_table.c.run_date  == run_date,
+                                       stats_table.c.dag_name  == dag_name,
+                                       stats_table.c.task_name == task_name)
+                               .values({ stats_table.c[column]: row_count })
+                               )
                 update_result = conn.execute(update_stmt)
-            except SQLAlchemyError as e:
-                print(f'Error: Could not UPDATE sys_etl_stats. Details: {e}')
-                raise
-            else:
-                conn.commit()
+
+    except SQLAlchemyError as e:
+        print(f'Error: Could not upsert stats for {task_name}. Details: {e}')
+        raise
